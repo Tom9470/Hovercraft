82830c303c612f3c173893277723d0334475f8e3
Tom9470 <69459755+Tom9470@users.noreply.github.com>
2021-02-07 14:12:02 +0000

    Update Hovercraft-master.py

--- Library/Hovercraft-master.py
+++ Library/Hovercraft-master.py
@@ -1,21 +1,35 @@
 import RPI.GPIO as GPIO
 import time
 
+
 class Hovercraft():
 
-    def __init__(lift_pin, left_thrust, right_thrust, rudder_pin):
+    def __init__(lift, lthrust, rthrust, rudder):
+
+        super().__init__()
+
+
         global lift_pin
-        if left_thrust != lift_pin:
+        lift_pin = lift
+        
+        if lthrust != lift:
             global left_thrust
+            left_thrust = lthrust
         else:
             return("left thrust is the same as one of the other pins")
-        if right_thrust != left_thrust and right_thrust != lift_pin
+
+        if rthrust != lthrust and rthrust != lift:
             global right_thrust
+            right_thrust = rthrust
         else:
             return("right thrust is the same as one of the other pins")
-        if isinstance(rudder_pin, int) and rudder_pin != right_thrust and rudder_pin != left_thrust and rudder_pin != lift_pin:
-            global rudder_pin
+
+        if rudder != rthrust and rudder != lthrust and rudder != lift:
+            if isinstance(rudder, int):
+                global rudder_pin
+                rudder_pin = rudder
         else:
             return("rudder pin is the same as one of the other pins")
-    
-    def
+
+
+    #def 
