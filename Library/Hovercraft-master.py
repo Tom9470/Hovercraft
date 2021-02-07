01f1609da0e1beb82a2c22b1b2999f194580b9ac
Tom9470 <69459755+Tom9470@users.noreply.github.com>
2021-02-07 17:56:44 +0000

    Update Hovercraft-master.py

--- Library/Hovercraft-master.py
+++ Library/Hovercraft-master.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-
+print("The UPHCL. (Universal Pi Hovercraft Control Library)\nSyntax for setup is Hovercraft(lift pin, left thrust pin, right thrust pin, rudder pin.).\n If you do not have a rudder, then put \"None\".\n for usage, refer to https://github.com/Tom9470/Hovercraft")
 
 class Hovercraft():
 
@@ -17,23 +17,29 @@ class Hovercraft():
             global left_thrust
             left_thrust = lthrust
             GPIO.setup(left_thrust, GPIO.OUT)
+
         else:
             return("left thrust is the same as one of the other pins")
+            GPIO.cleanup()
 
         if rthrust != lthrust and rthrust != lift:
             global right_thrust
             right_thrust = rthrust
-            GPIO.setup(lift_pin, GPIO.OUT)
+            GPIO.setup(right_thrust, GPIO.OUT)
         else:
             return("right thrust is the same as one of the other pins")
+            GPIO.cleanup()
 
         if rudder != rthrust and rudder != lthrust and rudder != lift:
             if isinstance(rudder, int):
+                global is_rudder_pin
+                is_rudder_pin = 1
                 global rudder_pin
                 rudder_pin = rudder
-                GPIO.setup(lift_pin, GPIO.OUT)
+                GPIO.setup(rudder_pin, GPIO.OUT)
         else:
             return("rudder pin is the same as one of the other pins")
+            GPIO.cleanup()
 
 
     #def 
