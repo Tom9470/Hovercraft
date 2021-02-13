5e63215cad17f25194a291b9e437237639e79397
Tom9470 <69459755+Tom9470@users.noreply.github.com>
2021-02-13 20:56:28 +0000

    Update hovercraft.py

--- Library/hovercraft.py
+++ Library/hovercraft.py
@@ -1,18 +1,23 @@
 import RPi.GPIO as GPIO
 import time
-print("The UPHCL. (Universal Pi Hovercraft Control Library)\nSyntax for setup is Hovercraft(lift pin, left thrust pin, right thrust pin, rudder pin.).\n If you do not have a rudder, then put \"None\".\n for usage, refer to https://github.com/Tom9470/Hovercraft")
+print("The UPHCL. (Universal Pi Hovercraft Control Library)\nSyntax for setup is Hovercraft(lift pin, maximum lift power in percent, left thrust pin, right thrust pin, rudder pin.).\n If you do not have a rudder, then put \"None\".\n for usage, refer to https://github.com/Tom9470/Hovercraft")
 
-def __init__(lift, lthrust, rthrust, rudder):
+def setup(lift, maxlift, lthrust, rthrust, rudder):
 
     GPIO.setmode(GPIO.BOARD)
     global lift_pin
-    lift_pin = lift
-    GPIO.setup(lift_pin, GPIO.OUT)
+    global maxlift
+    lift_pin = GPIO.setup(lift, GPIO.OUT)
+    GPIO.output(lift_pin, GPIO.LOW)
+    liftpwm = GPIO.PWM(lift_pin, 100)
+    liftpwm.start(0)
        
     if lthrust != lift:
         global left_thrust
-        left_thrust = lthrust
-        GPIO.setup(left_thrust, GPIO.OUT)
+        left_thrust = GPIO.setup(lthrust, GPIO.OUT)
+        GPIO.output(left_thrust, GPIO.LOW)
+        ltpwm = GPIO.PWM(left_thrust, 100)
+        ltpwm.start(0)
 
     else:
         return("left thrust is the same as one of the other pins")
@@ -20,22 +25,37 @@ def __init__(lift, lthrust, rthrust, rudder):
 
     if rthrust != lthrust and rthrust != lift:
         global right_thrust
-        right_thrust = rthrust
-        GPIO.setup(right_thrust, GPIO.OUT)
+        right_thrust = GPIO.setup(rthrust, GPIO.OUT)
+        GPIO.output(right_thrust, GPIO.LOW)
+        rtpwm = GPIO.PWM(right_thrust, 100)
+        rtpwm.start(0)
+
     else:
         return("right thrust is the same as one of the other pins")
         GPIO.cleanup()
 
     if rudder != rthrust and rudder != lthrust and rudder != lift:
+        global is_rudder_pin
         if isinstance(rudder, int):
-            global is_rudder_pin
             is_rudder_pin = 1
             global rudder_pin
-            rudder_pin = rudder
-            GPIO.setup(rudder_pin, GPIO.OUT)
+            rudder_pin = GPIO.setup(rudder, GPIO.OUT)
+            yawpwm = GPIO.PWM(rudder_pin, 100)
+            yawpwm.start(0)
+        
+        else:
+            is_rudder_pin = 0
+
     else:
         return("rudder pin is the same as one of the other pins")
         GPIO.cleanup()
     
     def startup():
-        
+        for PW in range(0, maxlift, 1):
+            pwm.ChangeDutyCycle(PW)
+            time.sleep(0.05)
+    
+    def shutdown():
+        pwm.ChangeDutyCycle(0)
+    
+    
